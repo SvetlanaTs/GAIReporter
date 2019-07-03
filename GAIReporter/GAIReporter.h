82d06206bf90a9fd1558d0db69466671fb538a82
Svetlana Tsetsorina <svetlana.tsetsorina@goods.ru>
2019-07-03 17:28:30 +0300

    Initial Commit

---
+++ GAIReporter/GAIReporter.h
@@ -0,0 +1,19 @@
+//
+//  GAIReporter.h
+//  GAIReporter
+//
+//  Created by Svetlana Tsetsorina on 03/07/2019.
+//  Copyright © 2019 Svetlana Tsetsorina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GAIReporter.
+FOUNDATION_EXPORT double GAIReporterVersionNumber;
+
+//! Project version string for GAIReporter.
+FOUNDATION_EXPORT const unsigned char GAIReporterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GAIReporter/PublicHeader.h>
+
+
